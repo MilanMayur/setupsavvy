a36dfb61084b9f3e7c6c0372654f10a41981b331
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-08-26 20:19:31 +0530

    Create adUnit.tsx

---
+++ components/adUnit.tsx
@@ -0,0 +1,37 @@
+//components/adUnit.tsx
+"use client";
+
+import { useEffect } from "react";
+
+declare global {
+    interface Window {
+        adsbygoogle: any[];
+    }
+}
+
+interface AdUnitProps {
+    slot: string; 
+    format?: string;
+    responsive?: boolean;
+}
+
+export default function AdUnit({ slot, format = "auto", responsive = true }: AdUnitProps) {
+    useEffect(() => {
+        try {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (e) {
+            console.error("AdSense error:", e);
+        }
+    }, []);
+
+    return (
+        <ins
+            className="adsbygoogle block my-4"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-1041356533581191"
+            data-ad-slot={slot}
+            data-ad-format={format}
+            data-full-width-responsive={responsive ? "true" : "false"}
+        ></ins>
+    );
+}
