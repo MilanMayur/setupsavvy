2911f925338f09c73d0058b6f25ce93ed4057c6a
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-08-27 16:31:24 +0530

    Update products.ts

--- data/products.ts
+++ data/products.ts
@@ -4,10 +4,12 @@ import chairs from "./chairs.json";
 import tables from "./tables.json";
 import laptops from "./laptops-5-60000.json";
 import keyboards from "./keyboards-10-4000.json";
+import mouse from "./mouse-10-4000.json"
 
 export const products = [
     ...headsets,
     ...keyboards,
+    ...mouse,
     ...chairs,
     ...tables,
     ...laptops,
