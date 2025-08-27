116c25861d4ec72159eda15bc6ff232699f6e6fa
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-08-27 16:24:55 +0530

    Create faqSection.tsx

---
+++ components/faqSection.tsx
@@ -0,0 +1,34 @@
+//components/faqSection.tsx
+"use client";
+import { useState } from "react";
+
+type Faq = {
+    question: string;
+    answer: string;
+};
+
+export default function FaqSection({ faqs }: { faqs: Faq[] }) {
+    const [openFaq, setOpenFaq] = useState<number | null>(null);
+
+    return (
+        <div className="mt-6">
+            <h2 className="text-xl font-bold mb-4">FAQs</h2>
+            <div className="space-y-3">
+            {faqs.map((faq, index) => (
+                <div key={index} className="border bg-gray-900 rounded-lg p-4 shadow-sm">
+                    <button
+                        onClick={() => setOpenFaq(openFaq === index ? null : index)}
+                        className="w-full text-left font-semibold flex justify-between"
+                    >
+                        {faq.question}
+                        <span>{openFaq === index ? "−" : "+"}</span>
+                    </button>
+                    {openFaq === index && (
+                    <p className="mt-2 text-gray-200">{faq.answer}</p>
+                    )}
+                </div>
+            ))}
+            </div>
+        </div>
+    );
+}
