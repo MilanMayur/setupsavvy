dc52924c1af9e398b3dce8b648610c3454e8f5b3
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-08-27 18:23:14 +0530

    Update blogCard.tsx

--- components/blogCard.tsx
+++ components/blogCard.tsx
@@ -1,6 +1,7 @@
 //components/blogCard.tsx
 import Image from "next/image";
 import Link from "next/link";
+import { Star } from "lucide-react";
 
 interface ProductProps {
     id: string;
@@ -15,27 +16,29 @@ interface ProductProps {
 
 export default function BlogCard({ id, name, price, url, image, pros, cons, rating }: ProductProps) {
     return (
-        <div className="bg-white shadow-md rounded-2xl p-4 max-w-sm">
+        <div className="bg-white border shadow-md hover:shadow-2xl rounded-2xl p-4 max-w-sm">
             <Link href={`/products/${id}`}>
-                <div className="min-h-[170px]">
+                <div className="min-h-[170px] items-center justify-center">
                     <Image
                         src={image}
                         alt={name}
                         width={400}
                         height={400}
-                        className="text-gray-400 rounded-lg object-cover mx-auto"
+                        className="text-gray-400 rounded-lg object-contain max-h-full"
                     />
                 </div>
 
-                <h2 className="text-lg text-black font-bold mt-3">{name}</h2>
-                <p className="text-green-600 font-semibold">₹ {price}</p>
-                <p className="text-orange-600 font-semibold">☆ {rating}</p>
+                <h2 className="text-lg text-black font-bold mt-3 line-clamp-2">{name}</h2>
+                <div className="flex items-center justify-between mt-1">
+                    <p className="text-green-600 font-semibold">₹ {price}</p>
+                    <p className="text-orange-600 font-semibold">☆ {rating}</p>
+                </div>
             </Link>
 
             <Link
                 href={url}
                 target="_blank"
-                className="inline-block bg-blue-600 text-white px-8 py-3 rounded-lg font-medium hover:bg-blue-700 transition"
+                className="block bg-blue-600 text-white text-center px-8 py-3 rounded-lg font-medium hover:bg-blue-700 transition"
             >
                 Buy on Amazon
             </Link>
@@ -60,5 +63,3 @@ export default function BlogCard({ id, name, price, url, image, pros, cons, rati
         </div>
     );
 }
-
-
