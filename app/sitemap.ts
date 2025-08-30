bfdfa7963871748280bee67367a8f84d329d1953
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-08-30 19:32:45 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,11 @@
 import { MetadataRoute } from "next";
 import blogs from "@/data/blogs.json"; 
 
+interface BlogPost {
+    slug: string;
+    date?: string;
+}
+
 export default function sitemap(): MetadataRoute.Sitemap {
 
     return [
@@ -11,7 +16,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: "weekly" as const,
             priority: 1.0,
         },
-        ...blogs.map((post: any) => ({
+        ...blogs.map((post: BlogPost) => ({
             url: `https://www.setupsavvy.com/blog/${post.slug}`,
             lastModified: post.date ? new Date(post.date) : new Date(),
             changeFrequency: "weekly" as const,
