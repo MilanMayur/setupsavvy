9182bddc78f54272bb75e08a4d85ec7f6b7578b8
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-09-04 00:22:59 +0530

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -12,6 +12,14 @@ export default function Head() {
                 name="google-adsense-account"
                 content="ca-pub-1041356533581191"
             />
+
+            {/* Favicons */}
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <meta name="theme-color" content="#2563eb" />
   
             {/* Open Graph */}
             <meta 
