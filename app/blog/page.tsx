211409a71d1a17648bf6745063c2e6dcd72077c0
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-09-16 00:30:49 +0530

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -16,8 +16,8 @@ type SearchParamsType = {
 
 const BLOGS_PER_PAGE = 15;
 
-export default async function BlogIndex({ searchParams }: { searchParams: SearchParamsType }) {
-    const params = await searchParams;
+export default function BlogIndex({ searchParams }: { searchParams: SearchParamsType }) {
+    const params = searchParams;
     const currentPage = Number(params.page) || 1;
 
     const today = new Date();
@@ -74,6 +74,7 @@ export default async function BlogIndex({ searchParams }: { searchParams: Search
         </section>
     );
 }
+
 
 
 
