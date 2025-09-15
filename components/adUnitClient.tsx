7e8df30327f9b48697ef12ecdf85700d24bda33c
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-09-15 23:36:19 +0530

    Update adUnitClient.tsx

--- components/adUnitClient.tsx
+++ components/adUnitClient.tsx
@@ -2,10 +2,9 @@
 "use client";
 import dynamic from "next/dynamic";
 import React from "react";
+import type { AdUnitProps } from "./adUnit";
 
 const AdUnit = dynamic(() => import("./adUnit"), { ssr: false });
-
-type AdUnitProps = React.ComponentProps<typeof AdUnit>;
 
 export default function AdUnitClient(props: AdUnitProps) {
     return <AdUnit {...props} />;
