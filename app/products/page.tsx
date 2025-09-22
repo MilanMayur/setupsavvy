05aed21d21cac1cc0aef346af947868b69543248
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-09-22 13:06:36 +0530

    Update Pagination component with query parameters
    Added query parameters to Pagination component for filtering.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -58,6 +58,7 @@ export default async function ProductsPage({ searchParams }: { searchParams: Pro
                 currentPage={page}
                 totalPages={totalPages}
                 basePath="/products"
+                queryParams={{ category: category !== "All" ? category : undefined, sort }}
             />
 
             {/* AdSense Ad Unit */}
@@ -80,6 +81,7 @@ export default async function ProductsPage({ searchParams }: { searchParams: Pro
         </section>
     );
 }
+
 
 
 
